b0b4b85a534c8cdf6d615edb3d2312e0c803ac32
sanggoro <philtyphils@gmail.com>
2020-11-11 16:50:07 +0700

    excel reporting

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -26,20 +26,99 @@ class Export extends CI_Controller
 	
 	public function index()
 	{
-        $data = $this->Export_model->getProvinsi();
+        //sample paramter
+        $provinsi_id = array();
+        $kategori_id = array();
+        $wilayah_kerja = array();
+        $data = $this->Export_model->getData($provinsi_id,$kategori_id,$wilayah_kerja);
+        //echo "<pre>";print_r($data->result_array());die();
         $spreadsheet = new Spreadsheet();
         $index = 0;
+        $wilayah = "";$row = 8;$no = 0;
+        $wilayah_kerja = "";
         foreach($data->result_array() as $r)
         {
-            $myWorkSheet = new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, $r['nama']);
-            $spreadsheet->addSheet($myWorkSheet, $index);
-            $sheet = $spreadsheet->setActiveSheetIndex($index);
-            $sheet->setCellValue('A1', 'No');
-		    $sheet->setCellValue('B1', 'Nama');
-		    $sheet->setCellValue('C1', 'Kelas');
-		    $sheet->setCellValue('D1', 'Jenis Kelamin');
-            $sheet->setCellValue('E1', 'Alamat');
-            $index++;
+            //style FONT CENTER * BOLD
+            $style = array(
+                'font' => array(
+                    'bold' => true,
+                ),
+                'alignment' => array(
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                )
+            );
+            if($wilayah != $r['provinsi'])
+            {
+                #title EXCEL
+                $myWorkSheet = new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, $r['provinsi']);
+                $spreadsheet->addSheet($myWorkSheet, $index);
+                $sheet = $spreadsheet->setActiveSheetIndex($index);
+                $sheet->mergeCells('B3:O3');
+                $sheet->getStyle("B3")->applyFromArray($style);
+                $sheet->mergeCells('B4:O4');
+                $sheet->getStyle("B4")->applyFromArray($style);
+                $sheet->mergeCells('B5:O5');
+                $sheet->getStyle("B5")->applyFromArray($style);
+                $sheet->setCellValue('B3', 'DAFTAR TERSUS & TUKS DI WILAYAH KERJA');
+                $sheet->setCellValue('B4', 'KANTOR UPT DITJEN HUBLA');
+                $sheet->setCellValue('B5', 'PROVINSI '.$r['provinsi']);
+                $index++; $row = 8;
+            }
+
+            // wilayah kerja
+            if($wilayah_kerja != $r['wilayah_kerja'])
+            {
+                $row += 3;
+                //WILAYAH KERJA
+                $wilayah_kerja = $r['wilayah_kerja'];
+                $sheet->setCellValue("C".$row, $wilayah_kerja);$row++;
+                //HEADER TABLE
+                $sheet->setCellValue("B".$row, 'No');
+                $sheet->setCellValue("C".$row, 'NAMA PERUSAHAAN');
+                $sheet->setCellValue("D".$row, 'BIDANG USAHA');
+                $sheet->setCellValue("E".$row, 'KATEGORI');
+                $sheet->setCellValue("F".$row, 'LOKASI');
+                $sheet->setCellValue("G".$row, 'ALAMAT');
+                $sheet->setCellValue("H".$row, 'PENANGGUNG JAWAB');
+                $sheet->setCellValue("I".$row, 'NPWP');
+                $sheet->setCellValue("J".$row, 'KOORDINAT');
+                $sheet->setCellValue("K".$row, 'TERSUS/TUKS');
+                $sheet->setCellValue("L".$row, 'SPESIFIKASI');
+                $sheet->setCellValue("M".$row, 'LEGALITAS');
+                $sheet->setCellValue("N".$row, 'TANGGAL TERBIT');
+                $sheet->setCellValue("O".$row, 'STATUS');
+                $sheet->setCellValue("P".$row, 'MASA BERLAKU');
+
+                #indexing autoincrement number
+                $no = 1;
+            }
+            #parameter new table
+            $wilayah_kerja = $r['wilayah_kerja'];
+            
+            $col = "B";$row++;
+		    $sheet->setCellValue($col.$row,$no);$col++;
+            $sheet->setCellValue($col.$row,$r['perusahaan']);$col++; #perusahaan
+            $sheet->setCellValue($col.$row,$r['bidang_usaha']);$col++; #bisang usaha
+            $sheet->setCellValue($col.$row,$r['kategori']);$col++; #kategori
+            $sheet->setCellValue($col.$row,$r['lokasi']);$col++;    # lokasi    
+            $sheet->setCellValue($col.$row,$r['alamat']);$col++; # alamat
+            $sheet->setCellValue($col.$row,$r['png_jwb']);$col++; #penganggung jawab
+            $sheet->setCellValue($col.$row,$r['npwp']);$col++; #npwp
+            $sheet->setCellValue($col.$row,$r['koordinat']);$col++; #koordinat
+            $sheet->setCellValue($col.$row,$r['ter_tuk']);$col++; #ter_tuk
+            $sheet->setCellValue($col.$row,$r['spesifikasi']);$col++; #spesifikasi
+            $sheet->setCellValue($col.$row,$r['legalitas']);$col++; #legalitas
+            $sheet->setCellValue($col.$row,$r['tgl_terbit']);$col++; #tanggal terbit
+            $status = "TIDAK AKTIF";
+            if(strtoupper($r['status']) == "Y")
+            {
+                $status = "AKTIF";
+            }
+            $sheet->setCellValue($col.$row,$status);$col++;
+
+            $sheet->setCellValue($col.$row,$r['ms_berlaku']);$col++;
+            $wilayah = $r['provinsi'];$no++;
+
         }
 		
 		
--- application/models/Export_model.php
+++ application/models/Export_model.php
@@ -7,7 +7,71 @@
 
         public function getProvinsi()
         {
-            $data = $this->db->where("LENGTH(kode)",2)->get('wilayah_2020');
+            $data = $this->db->where("LENGTH(kode)",2)->get('wilayah');
+            return $data;
+        }
+
+        public function getData($provinsi_id,$kategori_id,$wilayah_kerja)
+        {
+            $no = 0;
+            if(count($kategori_id) > 0)
+            {
+                foreach($provinsi_id as $p)
+                {
+                    if($no > 0)
+                    {
+                        $this->db->or_where("daftar_perusahaan.provinsi_id",$p);
+                    }
+                    else
+                    {
+                        $this->db->where("daftar_perusahaan.provinsi_id",$p);
+                    }
+                    $no++;
+                }
+            }
+            
+            if(count($kategori_id) > 0)
+            {
+                foreach($kategori_id as $k)
+                {
+                    $this->db->where("daftar_perusahaan.kategori_id",$k);
+                }
+            }
+
+            if(count($wilayah_kerja) > 0)
+            {
+                foreach($wilayah_kerja as $w)
+                {
+                    $this->db->where("daftar_perusahaan.bdgusaha_id",$w);
+                }
+            }
+
+            $this->db->where("LENGTH(wilayah.kode)","2");
+            $this->db->join("wilayah","daftar_perusahaan.provinsi_id=wilayah.kode");
+            $this->db->join("ksop","daftar_perusahaan.ksop_id=ksop.ksop_id");
+            $this->db->join("bdg_usaha","daftar_perusahaan.bdgusaha_id=bdg_usaha.bdg_usaha_id");
+            $this->db->join("kategori","daftar_perusahaan.kategori_id=kategori.kategori_id");
+            $this->db->select("
+                wilayah.nama as provinsi,
+                ksop.nama as wilayah_kerja,
+                daftar_perusahaan.nm_perusahaan as perusahaan,
+                bdg_usaha.nama as bidang_usaha,
+                kategori.nama as kategori,
+                daftar_perusahaan.lokasi as lokasi,
+                daftar_perusahaan.alamat as alamat,
+                daftar_perusahaan.png_jwb as png_jwb,
+                daftar_perusahaan.npwp as npwp,
+                daftar_perusahaan.koordinat_dd as koordinat,
+                daftar_perusahaan.ter_tuk as ter_tuk,
+                daftar_perusahaan.spesifikasi as spesifikasi,
+                daftar_perusahaan.sk as legalitas,
+                daftar_perusahaan.tgl_terbit as tgl_terbit,
+                daftar_perusahaan.status as status,
+                daftar_perusahaan.ms_berlaku as ms_berlaku
+            ");
+            $this->db->order_by("daftar_perusahaan.provinsi_id","asc");
+            $this->db->order_by("daftar_perusahaan.ksop_id","asc");
+            $data = $this->db->get("daftar_perusahaan");
             return $data;
         }
 		
